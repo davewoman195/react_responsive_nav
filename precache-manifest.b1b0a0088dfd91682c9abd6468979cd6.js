f67ef4ebdcced35828a2b18a68bd8ed46f5499dd
davewoman195 <67042217+davewoman195@users.noreply.github.com>
2020-09-10 14:26:48 +0530

    Add files via upload

---
+++ precache-manifest.b1b0a0088dfd91682c9abd6468979cd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eef1c6ccc4a9b7529a9adbed271ad4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ef6d5d7b9089f122ae",
+    "url": "/static/js/2.9ebcaddb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ebcaddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fa5287854b3be4eac5",
+    "url": "/static/js/main.e59a3c6d.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  }
+]);
